0367bbbb36f9bb28b8ca71a96175cd6c6679d3b7
Kunihiko Ito <kuni-ito@esm.co.jp>
2013-12-18 00:08:26 +0900

    Fix how to get the git repository

--- app/controllers/rgitlog/rgitlog_controller.rb
+++ app/controllers/rgitlog/rgitlog_controller.rb
@@ -4,7 +4,19 @@ require 'grit'
 module Rgitlog
   class RgitlogController < ApplicationController
     def index
-      @commits = Grit::Repo.new('.').commits
+      @commits = repo.commits
+    end
+
+    private
+    def repo dir = Dir.pwd
+      parent = parent_dir(dir)
+      return [] if dir == parent
+      return Grit::Repo.new(dir) if Dir.entries(dir).include?('.git')
+      repo(parent)
+    end
+
+    def parent_dir currrent_dir
+      File.expand_path('..', currrent_dir)
     end
   end
 end
